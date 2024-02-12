e67196aa4354f618f8c95c5e2cd16b08ddf16774
sairash <gautamsairash@gmail.com>
2024-02-13 03:18:07 +0545

    Added User Migration

---
+++ migrations/m240212_203619_create_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m240212_203619_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%users}}', [
+            'id' => $this->primaryKey(),
+            'email' => $this->string(255)->notNull()->unique(),
+            'password' => $this->string()->notNull(),
+            'auth_key' => $this->string(),
+            'access_token' => $this->string(),
+            'created_at' => $this->timestamp()
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
