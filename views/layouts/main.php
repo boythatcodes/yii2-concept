b395a8842271afd6e52379dcd6318f1d903dc8fa
sairash <gautamsairash@gmail.com>
2024-02-16 04:40:11 +0545

    removed footer

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -27,54 +27,27 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
     <title><?= Html::encode($this->title) ?></title>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="d-flex flex-column h-100">
+<body >
 <?php $this->beginBody() ?>
 
 <main id="main" class="flex-shrink-0" role="main">
-    <div class="container">
         <?= $content ?>
     </div>
 </main>
 
-<footer class="footer mt-auto w-full">
-    <div class="flex w-full justify-center">
-        <div class="w-full"></div>
-        <div class=" flex w-full" >
-            <div class="w-96 p-3 text-muted " style="background-color: #f5f5f5;">
-            <p class="text-2xl font-bold">Contact Us</p>
-            <p>Globalbizsoft </p>
-            <p class="flex"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 mt-.5"> <path fill-rule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd" /> </svg> 17420 NE 119TH Way, Redmond, WA 98052</p>
-            <p class="flex"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 mt-.5"><path fill-rule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clip-rule="evenodd" /> </svg> +14083733167</p>
-            </div>
-            
-        </div>
-        <div class="w-full"></div>
-        <div class="w-full"></div>
-    </div>
-</footer>
+
 
 <style>
-    main > .container {
-        padding: 70px 15px 20px;
-    }
-    @media (min-width: 768px){
-        .container, .container-sm, .container-md {
-            max-width: 720px;
-        }
-    }
-
-    @media (min-width: 576px){
-        .container, .container-sm {
-            max-width: 540px;
-        }
-    }
+    
     .footer {
         display: block;
         font-size: .9em;
         height: 130px;
         position: fixed;
         bottom: 0;
+        left: 0;
     }
 </style>
 
